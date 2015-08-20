d75459db4ef2a200af3dacf9db78efbfbb635e72
mvilner <shootermv@gmail.com>
2015-08-20 09:43:50 +0300

    1change

--- tasks/create-config.js
+++ tasks/create-config.js
@@ -15,26 +15,15 @@ module.exports = function(grunt) {
 
   grunt.registerMultiTask('create-config', 'creates config file', function() {
     // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
+   /* var options = this.options({
       punctuation: '.',
       separator: ', '
-    });
-
+    });*/
+     console.log('this.options - ',this.options().dest);
     // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
+ 
       // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
-        }
-      }).map(function(filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
+      var src ;
 
       // Handle options.
       src += '{\n'+
@@ -57,11 +46,11 @@ module.exports = function(grunt) {
 		'}';
 
       // Write the destination file.
-      grunt.file.write(f.dest, src);
+      grunt.file.write(this.options().dest, src);
 
       // Print a success message.
-      grunt.log.writeln('File "' + f.dest + '" created.');
-    });
+      grunt.log.writeln('File "' + this.options().dest + '" created.');
+  
   });
 
 };
